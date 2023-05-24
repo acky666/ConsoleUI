16f76b7759fff2f8f1dbba41272221593455b3e7
Ackers <mark@ackroyd.net>
2023-05-24 11:58:11 +0100

    Update

--- drawBox.go
+++ drawBox.go
@@ -1,5 +1,18 @@
 package ConsoleUI
 
-func drawBox(x int, y int, w int, h int, style string) {
+import "strings"
 
+func DrawBox(x int, y int, w int, h int, style string) {
+    
+    MoveTo(x, y)
+    
+    // Go through all the Items and find the max length
+    maxLength := 1
+    
+    // Draw a Box Starting at X, Y with the MaxLength
+    Out(myStyle.TopLeft)
+    Out(myStyle.HeaderText)
+    Out(strings.Repeat(myStyle.TopCenter, (len(myStyle.Gap)*2)+maxLength-(len(myStyle.HeaderText))))
+    Out(myStyle.TopRight)
+    
 }
